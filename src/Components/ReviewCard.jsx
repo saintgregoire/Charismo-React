fa7802b5957f8b58ced9a7ced08eee75b512ef16
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-08-23 15:56:55 +0200

    refact: Component  was creted, reviews.json was added and reviews section was rebuilded with fetch

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import style from './ReviewCard.module.scss'
 
-function ReviewCard() {
+import { FaStar } from "react-icons/fa"
+
+function ReviewCard({name, job, title, text, stars, photo}) {
+
+  const renderStars = () => {
+    return Array.from({length: stars}, (_, index) => (
+      <FaStar key={index}/>
+    ))
+  }
+
   return (
-    <div>ReviewCard</div>
+    <div className={`${style.swiper_slide}`}>
+      <div className={`${style.swiper_slide__top}`}>
+        <div className={`${style.swiper_slide__img}`}>
+          <img src={photo} alt={name} />
+        </div>
+        <div className={`${style.swiper_slide__client_info}`}>
+          <h3>{name}</h3>
+          <p>{job}</p>
+          <div className={`${style.swiper_slide__stars}`}>
+            {renderStars()}
+          </div>
+        </div>
+      </div>
+      <h3 className={`${style.swiper_slide__h3}`}>{title}</h3>
+      <p>{text}</p>
+    </div>
   )
 }
 
--- src/Sections/Reviews.jsx
+++ src/Sections/Reviews.jsx
@@ -1,32 +1,52 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import ReviewCard from '../Components/ReviewCard.jsx';
 
 import 'swiper/css'
 import 'swiper/css/pagination';
 import style from './Reviews.module.scss'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide  } from 'swiper/react'
 import { Pagination } from 'swiper/modules'
-
-import { FaStar } from "react-icons/fa"
-import maksym from '../assets/images/Maksym.webp'
-import emily from '../assets/images/Emily.webp'
-import john from '../assets/images/John.webp'
-import gael from '../assets/images/gael.webp'
 
 
 function Reviews() {
 
-  let slides = 1;
+  const [data, setData] = useState([]);
+  const [slides, setSlides] = useState(1);
 
-  if (window.innerWidth > 767){
-    slides = 2
-  }
-  if(window.innerWidth > 1439){
-    slides = 3
-  }
-  if(window.innerWidth > 1919){
-    slides = 4
-  }
+  const updateSlides = () => {
+    if (window.innerWidth > 1919) {
+      setSlides(4);
+    } else if (window.innerWidth > 1439) {
+      setSlides(3);
+    } else if (window.innerWidth > 767) {
+      setSlides(2);
+    } else {
+      setSlides(1);
+    }
+  };
+
+  useEffect(() => {
+    fetch('/data/reviews.json')
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((jsonData) => {
+        setData(jsonData);
+      })
+      .catch((error) => {
+        console.error('There was a problem width fetch:', error);
+      })
+
+    updateSlides();
+
+    window.addEventListener('resize', updateSlides);
+
+    return () => {
+      window.removeEventListener('resize', updateSlides);
+    };
+  }, []);
+  
 
   return (
     <section className={`${style.reviews}`}>
@@ -34,76 +54,23 @@ function Reviews() {
         <h2>Честные отзывы</h2>
         <p className={`${style.reviews__par}`}>клиентов, партнёров и профессионалов</p>
         <Swiper
-          className={`${style.swiper}`}
           modules={[Pagination]}
           spaceBetween={50}
           slidesPerView={slides}
           pagination={{ clickable: true }}
         >
-          <SwiperSlide className={`${style.swiper__slide}`}>
-            <div className={`${style.swiper__top}`}>
-              <div className={`${style.swiper__img}`}>
-                <img src={maksym} alt="фото клиента" />
-              </div>
-              <div className={`${style.swiper__client_info}`}>
-                  <h3>Максим Возничка</h3>
-                  <p>Веб Разработчик</p>
-                  <div className={`${style.swiper__stars}`}>
-                  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar />
-                  </div>
-              </div>
-            </div>
-            <h3 className={`${style.swiper__h3}`}>Прекрасно себя показал</h3>
-            <p>«Использую Декопил в своей практике несколько лет»</p>
-          </SwiperSlide>
-          <SwiperSlide className={`${style.swiper__slide}`}>
-            <div className={`${style.swiper__top}`}>
-              <div className={`${style.swiper__img}`}>
-                <img src={emily} alt="фото клиента" />
-              </div>
-              <div className={`${style.swiper__client_info}`}>
-                  <h3>Эмили Довольная</h3>
-                  <p>Косметолог</p>
-                  <div className={`${style.swiper__stars}`}>
-                  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar />
-                  </div>
-              </div>
-            </div>
-            <h3 className={`${style.swiper__h3}`}>Прекрасно себя показал</h3>
-            <p>«Использую Декопил в своей практике несколько лет»</p>
-          </SwiperSlide>
-          <SwiperSlide className={`${style.swiper__slide}`}>
-            <div className={`${style.swiper__top}`}>
-              <div className={`${style.swiper__img}`}>
-                <img src={john} alt="фото клиента" />
-              </div>
-              <div className={`${style.swiper__client_info}`}>
-                  <h3>Джон Пижон</h3>
-                  <p>Кинолог</p>
-                  <div className={`${style.swiper__stars}`}>
-                  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar />
-                  </div>
-              </div>
-            </div>
-            <h3 className={`${style.swiper__h3}`}>Прекрасно себя показал</h3>
-            <p>«Использую Декопил в своей практике несколько лет»</p>
-          </SwiperSlide>
-          <SwiperSlide className={`${style.swiper__slide}`}>
-            <div className={`${style.swiper__top}`}>
-              <div className={`${style.swiper__img}`}>
-                <img src={gael} alt="фото клиента" />
-              </div>
-              <div className={`${style.swiper__client_info}`}>
-                  <h3>Гаёль Фелюкс</h3>
-                  <p>Актер</p>
-                  <div className={`${style.swiper__stars}`}>
-                  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar />
-                  </div>
-              </div>
-            </div>
-            <h3 className={`${style.swiper__h3}`}>Прекрасно себя показал</h3>
-            <p>«Использую Декопил в своей практике несколько лет»</p>
-          </SwiperSlide>
+          {data.map((item) => (
+            <SwiperSlide key={item.id}>
+              <ReviewCard 
+              name={item.name}
+              job={item.job}
+              title={item.reviewTitle}
+              text={item.review}
+              stars={item.stars}
+              photo={item.photo}
+            />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
       
