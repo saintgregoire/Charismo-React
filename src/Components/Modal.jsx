2fa3bbbc66e6c970f10eda20a6c2300a85b350cd
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-09-02 15:19:26 +0200

    feat

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -4,7 +4,6 @@ import style from './Modal.module.scss'
 import { ModalContext } from '../Services/Context.jsx';
 
 import { IoIosClose } from 'react-icons/io';
-import { ModalContext } from '../Services/Context.jsx';
 
 
 
